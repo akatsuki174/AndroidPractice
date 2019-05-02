5c38c557db9bc8488c24d3fed705f02a36489019
akatsuki174 <dev2015js@gmail.com>
2019-05-02 17:08:06 +0900

    前の画面で渡されたテキストを、次の画面で表示するようにした

--- Practice/app/src/main/java/com/example/practice/DisplayMessageActivity.kt
+++ Practice/app/src/main/java/com/example/practice/DisplayMessageActivity.kt
@@ -2,11 +2,16 @@ package com.example.practice
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_display_message.*
+
 
 class DisplayMessageActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display_message)
+
+        val message = intent.getStringExtra(MainActivity().EXTRA_MESSAGE)
+        textView.text = message
     }
 }
