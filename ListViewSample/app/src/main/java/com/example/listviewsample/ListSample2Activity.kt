476c3077f96dbfcd0426373b605440d8d68ca038
akatsuki174 <dev2015js@gmail.com>
2019-05-12 22:30:22 +0900

    別の方法でListViewの要素を表示するようにした

--- ListViewSample/app/src/main/java/com/example/listviewsample/ListSample2Activity.kt
+++ ListViewSample/app/src/main/java/com/example/listviewsample/ListSample2Activity.kt
@@ -2,11 +2,28 @@ package com.example.listviewsample
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_list_sameple_2.*
 
 class ListSample2Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_sameple_2)
+
+        val menuList = arrayListOf<String>()
+        menuList.add("唐揚げ定食")
+        menuList.add("ハンバーグ定食")
+        menuList.add("生姜焼き定食")
+        menuList.add("ステーキ定食")
+        menuList.add("野菜炒め定食")
+        menuList.add("とんかつ定食")
+        menuList.add("ミンチかつ定食")
+        menuList.add("チキンカツ定食")
+        menuList.add("コロッケ定食")
+        menuList.add("焼き魚定食")
+        menuList.add("焼肉定食")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, menuList)
+        lvMenu.adapter = adapter
     }
 }
