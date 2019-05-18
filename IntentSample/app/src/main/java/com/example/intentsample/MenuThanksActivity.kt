bf44207030f1139a881a4a7cc8fc23f6e4f8200c
akatsuki174 <dev2015js@gmail.com>
2019-05-18 19:57:02 +0900

    タップされたアイテムが表示されるようにした

--- IntentSample/app/src/main/java/com/example/intentsample/MenuThanksActivity.kt
+++ IntentSample/app/src/main/java/com/example/intentsample/MenuThanksActivity.kt
@@ -2,11 +2,15 @@ package com.example.intentsample
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_menu_thanks.*
 
 class MenuThanksActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_thanks)
+
+        tvMenuName.text = intent.getStringExtra("menuName")
+        tvMenuPrice.text = intent.getStringExtra("menuPrice")
     }
 }
