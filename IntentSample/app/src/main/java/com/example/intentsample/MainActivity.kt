5a4b17cb3ad37c4c8f91c6f3dea745413c3eeda9
akatsuki174 <dev2015js@gmail.com>
2019-05-18 17:47:07 +0900

    リストビューを表示

--- IntentSample/app/src/main/java/com/example/intentsample/MainActivity.kt
+++ IntentSample/app/src/main/java/com/example/intentsample/MainActivity.kt
@@ -2,11 +2,40 @@ package com.example.intentsample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SimpleAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val menuList = arrayListOf<Map<String, String>>()
+        menuList.add(createMenu("唐揚げ定食", "850円"))
+        menuList.add(createMenu("焼き魚定食", "800円"))
+        menuList.add(createMenu("日替わり定食", "800円"))
+        menuList.add(createMenu("とんかつ定食", "850円"))
+        menuList.add(createMenu("ざるそば定食", "750円"))
+        menuList.add(createMenu("天ぷら定食", "850円"))
+        menuList.add(createMenu("親子丼定食", "850円"))
+        menuList.add(createMenu("牛丼定食", "1000円"))
+        menuList.add(createMenu("かき揚げ定食", "800円"))
+        menuList.add(createMenu("豚丼定食", "850円"))
+        menuList.add(createMenu("刺し身定食", "1000円"))
+        menuList.add(createMenu("餃子定食", "800円"))
+
+        val from: Array<String> = arrayOf("name", "price")
+        val to: IntArray = intArrayOf(android.R.id.text1, android.R.id.text2)
+        val adapter = SimpleAdapter(this, menuList, android.R.layout.simple_list_item_2, from, to)
+        lvMenu.adapter = adapter
+
+    }
+
+    private fun createMenu(name: String, price: String): Map<String, String> {
+        val map = hashMapOf<String, String>()
+        map.put("name", name)
+        map.put("price", price)
+        return map
     }
 }
