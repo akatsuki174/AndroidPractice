707ec0f652377073a2460f2c3ae75f8e29e32ec7
akatsuki174 <dev2015js@gmail.com>
2019-07-06 16:27:26 +0900

    ダミーデータを入れてListを表示

--- app/src/main/java/com/example/torutsumesamle/MainActivity.kt
+++ app/src/main/java/com/example/torutsumesamle/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.torutsumesamle
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.SimpleAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val items = listOf("aaa", "bbb", "ccc", "ddd", "eee", "fff", "ggg", "hhh", "iii", "jjj", "kkk", "lll", "mmm", "nnn", "ooo", "ppp")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, items)
+        listView.adapter = adapter
     }
 }
