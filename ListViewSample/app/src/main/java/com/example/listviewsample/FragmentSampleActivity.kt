40976e96700ebc0548f216af9a97ff06181d90c5
akatsuki174 <dev2015js@gmail.com>
2019-10-08 23:43:00 +0900

    Fragmentを1枚乗せて表示するようにした

--- ListViewSample/app/src/main/java/com/example/listviewsample/FragmentSampleActivity.kt
+++ ListViewSample/app/src/main/java/com/example/listviewsample/FragmentSampleActivity.kt
@@ -8,5 +8,13 @@ class FragmentSampleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fragment_sample)
+
+        if (savedInstanceState == null) {
+            val fragmentManager = supportFragmentManager
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.container, MenuListFragment())
+            fragmentTransaction.commit()
+        }
+
     }
 }
---
+++ ListViewSample/app/src/main/java/com/example/listviewsample/MenuListFragment.kt
@@ -0,0 +1,24 @@
+package com.example.listviewsample
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class MenuListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_menu_list, container, false)
+    }
+
+
+}
