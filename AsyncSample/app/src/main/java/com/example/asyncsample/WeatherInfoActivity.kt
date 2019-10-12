c4e7092ac26fe831f8d8b6027cb35078c1de3274
akatsuki174 <dev2015js@gmail.com>
2019-10-13 00:31:37 +0900

    新しいプロジェクトを追加

---
+++ AsyncSample/app/src/main/java/com/example/asyncsample/WeatherInfoActivity.kt
@@ -0,0 +1,12 @@
+package com.example.asyncsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WeatherInfoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather_info)
+    }
+}
