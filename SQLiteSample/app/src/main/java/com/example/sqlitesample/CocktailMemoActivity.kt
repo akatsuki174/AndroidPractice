f42ef374c43d76b353e22e1a7bac60acc184ef5c
akatsuki174 <dev2015js@gmail.com>
2019-10-12 23:14:17 +0900

    SQLiteに保存する処理、取得する処理を追加

--- SQLiteSample/app/src/main/java/com/example/sqlitesample/CocktailMemoActivity.kt
+++ SQLiteSample/app/src/main/java/com/example/sqlitesample/CocktailMemoActivity.kt
@@ -1,5 +1,6 @@
 package com.example.sqlitesample
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -15,6 +16,8 @@ class CocktailMemoActivity : AppCompatActivity() {
     private var tvCocktailName: TextView? = null
     private var btnSave: Button? = null
 
+    @Suppress("NAME_SHADOWING")
+    @SuppressLint("Recycle")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cocktail_memo)
@@ -27,11 +30,46 @@ class CocktailMemoActivity : AppCompatActivity() {
             cocktailName = parent.getItemAtPosition(position).toString()
             tvCocktailName?.text = cocktailName
             btnSave?.isEnabled = true
+
+            val helper = DatabaseHelper(this)
+            val db = helper.writableDatabase
+            db.use { db ->
+                val sql = "SELECT * FROM cocktailmemo WHERE _id = $cocktailId"
+                val cursor = db.rawQuery(sql, null)
+                var note = ""
+                while (cursor.moveToNext()) {
+                    val indexNote = cursor.getColumnIndex("note")
+                    note = cursor.getString(indexNote)
+                }
+                val etNote = findViewById<EditText>(R.id.etNote)
+                etNote.setText(note)
+            }
         }
     }
 
+    @Suppress("NAME_SHADOWING")
     fun onSaveButtonClick(view: View) {
         val etNote = findViewById<EditText>(R.id.etNote)
+        val note = etNote.text.toString()
+
+        val helper = DatabaseHelper(this)
+        val db = helper.writableDatabase
+
+        db.use { db ->
+            // すでに入っているデータを削除
+            val sqlDelete = "DELETE FROM cocktailmemo WHERE _id = ?"
+            var stmt = db.compileStatement(sqlDelete)
+            stmt.bindLong(1, cocktailId.toLong())
+            stmt.executeUpdateDelete()
+
+            // 新しいデータを追加
+            val sqlInsert = "INSERT INTO cocktailmemo (_id, name, note) VALUES (?, ?, ?)"
+            stmt = db.compileStatement(sqlInsert)
+            stmt.bindLong(1, cocktailId.toLong())
+            stmt.bindString(2, cocktailName)
+            stmt.bindString(3, note)
+            stmt.executeInsert()
+        }
         tvCocktailName?.text = getString(R.string.tv_name)
         etNote.setText("")
         btnSave?.isEnabled = false
