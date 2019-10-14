32f1151227b6b0f0ede1f88ab37f5fd3d86b0eb8
akatsuki174 <dev2015js@gmail.com>
2019-10-14 16:19:23 +0900

    メディア再生サンプルのUIを作成

---
+++ MediaSample/app/src/main/java/com/example/mediasample/MediaControlActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mediasample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MediaControlActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_media_control)
+    }
+}
