d03f1f11dc4a50f869fa3991c6d13d522b87587e
akatsuki174 <dev2015js@gmail.com>
2019-10-15 00:07:36 +0900

    地図のキーワード検索処理を追加

--- ImplicitIntentSample/app/src/main/java/com/example/implicitintentsample/IntentStartActivity.kt
+++ ImplicitIntentSample/app/src/main/java/com/example/implicitintentsample/IntentStartActivity.kt
@@ -1,12 +1,34 @@
 package com.example.implicitintentsample
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import java.io.UnsupportedEncodingException
+import java.net.URLEncoder
 
 class IntentStartActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intent_start)
+    }
+
+    fun onMapSearchButtonClick(view: View) {
+        val etSearchWord = findViewById<EditText>(R.id.etSearchWord)
+        var searchWord = etSearchWord.text.toString()
+
+        try {
+            searchWord = URLEncoder.encode(searchWord, "UTF-8")
+            val uriStr = "geo:0.0?q=$searchWord"
+            val uri = Uri.parse(uriStr)
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        } catch (ex: UnsupportedEncodingException) {
+            Log.e("IntentStartActivity", "検索キーワード変換失敗", ex)
+        }
     }
 }
