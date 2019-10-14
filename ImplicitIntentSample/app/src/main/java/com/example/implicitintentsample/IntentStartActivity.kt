32bb4c2c0e33374b482a48a0968fd431e3cf4c3c
akatsuki174 <dev2015js@gmail.com>
2019-10-14 23:14:32 +0900

    暗黙的インテントのサンプルプロジェクトの初期コミット

---
+++ ImplicitIntentSample/app/src/main/java/com/example/implicitintentsample/IntentStartActivity.kt
@@ -0,0 +1,12 @@
+package com.example.implicitintentsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IntentStartActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intent_start)
+    }
+}
