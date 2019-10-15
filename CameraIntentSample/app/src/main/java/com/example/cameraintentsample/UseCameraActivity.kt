3d1f303c2bae5ab61f54f3c4a9a98160362e78d8
akatsuki174 <dev2015js@gmail.com>
2019-10-15 14:14:14 +0900

    画像をストレージに保存して、それを画面に表示するようにした

--- CameraIntentSample/app/src/main/java/com/example/cameraintentsample/UseCameraActivity.kt
+++ CameraIntentSample/app/src/main/java/com/example/cameraintentsample/UseCameraActivity.kt
@@ -1,15 +1,24 @@
 package com.example.cameraintentsample
 
+import android.Manifest
 import android.app.Activity
+import android.content.ContentValues
 import android.content.Intent
-import android.graphics.Bitmap
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PatternMatcher
 import android.provider.MediaStore
 import android.view.View
 import android.widget.ImageView
+import androidx.core.app.ActivityCompat
+import java.text.SimpleDateFormat
+import java.util.*
 
 class UseCameraActivity : AppCompatActivity() {
+
+    private var imageUri: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,15 +28,42 @@ class UseCameraActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == 200 && resultCode == Activity.RESULT_OK) {
-            val bitmap = data?.getParcelableExtra<Bitmap>("data")
             val ivCamera = findViewById<ImageView>(R.id.ivCamera)
-            ivCamera.setImageBitmap(bitmap)
+            ivCamera.setImageURI(imageUri)
         }
     }
 
     fun onCameraImageClick(view: View) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+        ) {
+            val permissions = arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            ActivityCompat.requestPermissions(this, permissions, 2000)
+            return
+        }
+
+        val dateFormat = SimpleDateFormat("yyyyMMddHHmmss")
+        val now = Date(System.currentTimeMillis())
+        val nowStr = dateFormat.format(now)
+        val fileName = "UseCameraActivityPhoto_${nowStr}.jpg"
+        val values = ContentValues()
+        values.put(MediaStore.Images.Media.TITLE, fileName)
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+        val resolver = contentResolver
+        imageUri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
         val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
         startActivityForResult(intent, 200)
     }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 2000 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            val ivCamera = findViewById<ImageView>(R.id.ivCamera)
+            onCameraImageClick(ivCamera)
+        }
+    }
 }
