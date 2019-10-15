6c7647707f2de2fe7776bccb1c82d7a6f2902d69
akatsuki174 <dev2015js@gmail.com>
2019-10-15 14:58:07 +0900

    アクションバーではなくツールバーを使うようにした

--- ToolbarSample/app/src/main/java/com/example/toolbarsample/ScrollArticleActivity.kt
+++ ToolbarSample/app/src/main/java/com/example/toolbarsample/ScrollArticleActivity.kt
@@ -1,12 +1,22 @@
 package com.example.toolbarsample
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 
 class ScrollArticleActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scroll_article)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        toolbar.setLogo(R.mipmap.ic_launcher)
+        toolbar.setTitle(R.string.toolbar_title)
+        toolbar.setTitleTextColor(Color.WHITE)
+        toolbar.setSubtitle(R.string.toolbar_subtitle)
+        toolbar.setSubtitleTextColor(Color.LTGRAY)
+        setSupportActionBar(toolbar)
     }
 }
