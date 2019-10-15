350b998778fa570ea2e483b741e4d16eb7561f39
akatsuki174 <dev2015js@gmail.com>
2019-10-15 11:52:00 +0900

    カメラサンプルのためのプロジェクトを新規作成

---
+++ CameraIntentSample/app/src/main/java/com/example/cameraintentsample/UseCameraActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cameraintentsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UseCameraActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_use_camera)
+    }
+}
