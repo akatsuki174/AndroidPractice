f792f1303a8b2375eb4abab50c5e04d24fc16003
akatsuki174 <dev2015js@gmail.com>
2019-10-15 20:24:46 +0900

    RecyclerViewを配置した

--- RecyclerViewSample/app/src/main/java/com/example/recyclerviewsample/ScrollListActivity.kt
+++ RecyclerViewSample/app/src/main/java/com/example/recyclerviewsample/ScrollListActivity.kt
@@ -1,12 +1,23 @@
 package com.example.recyclerviewsample
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import com.google.android.material.appbar.CollapsingToolbarLayout
 
 class ScrollListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scroll_list)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        toolbar.setLogo(R.mipmap.ic_launcher)
+        setSupportActionBar(toolbar)
+        val toolbarLayout = findViewById<CollapsingToolbarLayout>(R.id.toolbarLayout)
+        toolbarLayout.title = getString(R.string.toolbar_title)
+        toolbarLayout.setExpandedTitleColor(Color.WHITE)
+        toolbarLayout.setCollapsedTitleTextColor(Color.LTGRAY)
     }
 }
