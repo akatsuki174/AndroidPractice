39298cdc020dec6d12fbb2017905f5b9e616a30e
akatsuki174 <dev2015js@gmail.com>
2019-10-15 11:51:41 +0900

    GPS機能を使って現在の位置を取得できるようにした

--- ImplicitIntentSample/app/src/main/java/com/example/implicitintentsample/IntentStartActivity.kt
+++ ImplicitIntentSample/app/src/main/java/com/example/implicitintentsample/IntentStartActivity.kt
@@ -1,20 +1,44 @@
 package com.example.implicitintentsample
 
+import android.Manifest
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.EditText
+import android.widget.TextView
+import androidx.core.app.ActivityCompat
 import java.io.UnsupportedEncodingException
 import java.net.URLEncoder
 
 class IntentStartActivity : AppCompatActivity() {
 
+    private var latitude: Double = 0.0
+    private var longitude: Double = 0.0
+    private var tvLatitude: TextView? = null
+    private var tvLongitude: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intent_start)
+
+        tvLatitude = findViewById(R.id.tvLatitude)
+        tvLongitude = findViewById(R.id.tvLongitude)
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        val locationListener = GPSLocationListener()
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            val permissions = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
+            ActivityCompat.requestPermissions(this, permissions, 1000)
+            return
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0.toFloat(), locationListener)
     }
 
     fun onMapSearchButtonClick(view: View) {
@@ -29,6 +53,50 @@ class IntentStartActivity : AppCompatActivity() {
             startActivity(intent)
         } catch (ex: UnsupportedEncodingException) {
             Log.e("IntentStartActivity", "検索キーワード変換失敗", ex)
+        }
+    }
+
+    fun onMapShowCurrentButtonClick(view: View) {
+        val uriStr = "geo:${latitude},${longitude}"
+        val uri = Uri.parse(uriStr)
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+        startActivity(intent)
+    }
+
+    private inner class GPSLocationListener: LocationListener {
+        override fun onProviderEnabled(p0: String?) {
+
+        }
+
+        override fun onProviderDisabled(p0: String?) {
+
+        }
+
+        override fun onStatusChanged(p0: String?, p1: Int, p2: Bundle?) {
+
+        }
+
+        override fun onLocationChanged(location: Location?) {
+            latitude = location?.latitude ?: 0.0
+            longitude = location?.longitude ?: 0.0
+            tvLongitude?.text = latitude.toString()
+            tvLongitude?.text = longitude.toString()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 1000 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            val locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+            val locationListener = GPSLocationListener()
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return
+            }
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0.toFloat(), locationListener)
         }
     }
 }
