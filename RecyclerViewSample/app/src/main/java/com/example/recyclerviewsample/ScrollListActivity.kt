15fbc9a43abda573029994965bf5a6787afd0937
akatsuki174 <dev2015js@gmail.com>
2019-10-15 20:02:21 +0900

    RecyclerViewのサンプルプロジェクトを新規作成

---
+++ RecyclerViewSample/app/src/main/java/com/example/recyclerviewsample/ScrollListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.recyclerviewsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScrollListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scroll_list)
+    }
+}
