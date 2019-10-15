fc6773a0008419fad0b5cbe470566cad2f90340b
akatsuki174 <dev2015js@gmail.com>
2019-10-15 12:16:47 +0900

    カメラを起動して撮影する処理を追加

--- CameraIntentSample/app/src/main/java/com/example/cameraintentsample/UseCameraActivity.kt
+++ CameraIntentSample/app/src/main/java/com/example/cameraintentsample/UseCameraActivity.kt
@@ -1,7 +1,13 @@
 package com.example.cameraintentsample
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.ImageView
 
 class UseCameraActivity : AppCompatActivity() {
 
@@ -9,4 +15,19 @@ class UseCameraActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_use_camera)
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 200 && resultCode == Activity.RESULT_OK) {
+            val bitmap = data?.getParcelableExtra<Bitmap>("data")
+            val ivCamera = findViewById<ImageView>(R.id.ivCamera)
+            ivCamera.setImageBitmap(bitmap)
+        }
+    }
+
+    fun onCameraImageClick(view: View) {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(intent, 200)
+    }
+
 }
