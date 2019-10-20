7a71a22ad846e0a99b1e7fc3a2ad8e9077281b04
akatsuki174 <dev2015js@gmail.com>
2019-10-20 19:55:48 +0900

    バリデーション処理を追加

--- Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,11 +2,33 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        calculate.setOnClickListener {
+            var isValid = true
+
+            val priceText = price.text.toString()
+            if (priceText.isEmpty()) {
+                price.error = getString(R.string.price_error)
+                isValid = false
+            }
+
+            val discountText = discount.text.toString()
+            if (discountText.isEmpty()) {
+                discount.error = getString(R.string.discount_error)
+                isValid = false
+            }
+
+            if (isValid) {
+                val priceValue = priceText.toInt()
+                val discountValue = discountText.toInt()
+            }
+        }
     }
 }
