dea4520e238e1a15d80da14b2b4779fea5dce3f3
akatsuki174 <dev2015js@gmail.com>
2019-10-22 18:50:22 +0900

    文字列をSharedPreferencesに保存、読み出す処理を追加

--- SharedPreferenceSample/app/src/main/java/com/example/sharedpreferencesample/MainActivity.kt
+++ SharedPreferenceSample/app/src/main/java/com/example/sharedpreferencesample/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.sharedpreferencesample
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val pref = getSharedPreferences("file_name", Context.MODE_PRIVATE)
+        val storedText = pref.getString("key", "未登録")
+        editText.setText(storedText)
+        button.setOnClickListener {
+            val inputText = editText.text.toString()
+            pref.edit().putString("key", inputText).apply()
+        }
     }
 }
