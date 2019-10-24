907af3ec725024a79cd4cd4d46cc53d471cef860
akatsuki174 <dev2015js@gmail.com>
2019-10-25 00:01:42 +0900

    選択した項目をもとに、リストを表示するようにした

--- WorldClock/app/src/main/java/com/example/worldclock/MainActivity.kt
+++ WorldClock/app/src/main/java/com/example/worldclock/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.worldclock
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
@@ -18,5 +22,29 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, TimeZoneSelectActivity::class.java)
             startActivityForResult(intent, 1)
         }
+
+        showWorldClocks()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1
+            && resultCode == Activity.RESULT_OK
+            && data != null
+        ) {
+            val timeZone = data.getStringExtra("timeZone")
+            val pref = getSharedPreferences("prefs", Context.MODE_PRIVATE)
+            val timeZones = pref.getStringSet("time_zone", mutableSetOf())?.toMutableSet()
+                ?: mutableSetOf()
+            timeZones.add(timeZone)
+            pref.edit().putStringSet("time_zone", timeZones.toSet()).apply()
+            showWorldClocks()
+        }
+    }
+
+    private fun showWorldClocks() {
+        val pref = getSharedPreferences("prefs", Context.MODE_PRIVATE)
+        val timeZones = pref.getStringSet("time_zone", null) ?: setOf()
+        clockList.adapter = TimeZoneAdapter(this, timeZones.toTypedArray())
     }
 }
